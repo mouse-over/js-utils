32ca1f8c9084deee90d47286cb6db77432ad37b8
Vaclav Prokes <vaclav.prokes@eset.cz>
2022-03-17 12:25:07 +0100

    getOnPath test

--- tests/getOnPath.test.js
+++ tests/getOnPath.test.js
@@ -11,3 +11,60 @@ test('getOnPath not existing value', () => {
 test('getOnPath not existing value container', () => {
     expect(getOnPath({foo: null}, ['foo','bar'])).toBeUndefined();
 });
+
+test('getOnPath children', () => {
+    const path = [
+        "children",
+        "expectedSolutions",
+        "children",
+        0,
+        "children",
+        "value",
+        "valid"
+    ];
+    const object = {
+        "children": {
+            "owner": {
+                "valid": false,
+                "messages": [
+                    "Není zadán koncový zákazník"
+                ]
+            },
+            "dateExpected": {
+                "valid": false,
+                "messages": [
+                    "Toto pole je vyžadováno!"
+                ]
+            },
+            "phaseId": {
+                "valid": false,
+                "messages": [
+                    "Toto pole je vyžadováno!"
+                ]
+            },
+            "successProbability": {
+                "valid": false,
+                "messages": [
+                    "Toto pole je vyžadováno!"
+                ]
+            },
+            "expectedSolutions": {
+                "valid": true,
+                "messages": [],
+                "children": {
+                    "0": {
+                        "children": {
+                            "value": {
+                                "valid": true,
+                                "messages": []
+                            }
+                        },
+                        "valid": true
+                    }
+                }
+            }
+        },
+        "valid": false
+    };
+    expect(getOnPath(object, path)).toBe(true);
+});
